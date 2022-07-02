b798e7935e78802d7e53a29b932b647ca22b13f5
Nazmul <csenazmul96@gmail.com>
2022-07-02 12:34:39 +0600

    show cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,33 +1,27 @@
+import {useContext} from "react";
 import classes from './Cart.module.css'
 import Modal from "../UI/Modal";
-
+import CartContext from "../../store/cartContext";
 const Cart = props =>{
+    const contaxt = useContext(CartContext)
+    const totalAmount = contaxt.totalAmount.toFixed(2)
     const cartItems = (
         <ul className={classes['cart-items']}>
-            {
-                [
-                    {
-                        id: 'c1',
-                        name: 'Sushi',
-                        amount: 2,
-                        price: 12.4
-                    }
-                ].map(item=>{
-                    <li>{item.name}</li>
-                })
-            }
+            {contaxt.items.map((item, index)=> {
+                return <li key={index}>{item.name}</li>
+            })}
         </ul>
-    )
+    );
     return (
         <Modal closeModal={props.closeClick}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>45.4</span>
+                <span>${totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.closeClick}>Close</button>
-                <button className={classes.button}>Order</button>
+                {contaxt.items.length ? <button className={classes.button}>Order</button> : null}
             </div>
         </Modal>
     )
--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -3,25 +3,25 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 const list = [
     {
-        id: 'm1',
+        id: 1,
         name: 'Sushi',
         description: 'Finest fish and veggies',
         price: 22.99,
     },
     {
-        id: 'm2',
+        id: 2,
         name: 'Schnitzel',
         description: 'A german specialty!',
         price: 16.5,
     },
     {
-        id: 'm3',
+        id: 3,
         name: 'Barbecue Burger',
         description: 'American, raw, meaty',
         price: 12.99,
     },
     {
-        id: 'm4',
+        id: 4,
         name: 'Green Bowl',
         description: 'Healthy...and green...',
         price: 18.99,
@@ -29,7 +29,7 @@ const list = [
 ];
 
 const AvalilableMeals = ()=>{
-    const mealLIst = list.map((meal=><MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id} />))
+    const mealLIst = list.map((meal=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id} />))
     return (
         <section className={classes.meals}>
             <Card>
